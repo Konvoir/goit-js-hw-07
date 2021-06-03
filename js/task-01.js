185d44066ce9e28b4c124fece49b08e351101ba8
Konvoir <baleskoff@gmail.com>
2021-06-03 13:36:01 +0300

    begin

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const sumCategories = document.querySelectorAll('.item').length;
+const message = `В списке ${sumCategories} категории.`;
+console.log(message);
+const heddingText = document.querySelectorAll('.item');
+// const tegs = heddingText.children.children
+console.log("🚀 ~ file: task-01.js ~ line 5 ~ heddingText", heddingText);
+// текст заголовка
---
+++ js/task-04.js
@@ -0,0 +1,8 @@
+const buttonDecrement = document.querySelector('.decrement');
+const buttonIncrement = document.querySelector('.increment');
+
+buttonDecrement.addEventListener('click', onButtonDecrement);
+buttonIncrement.addEventListener('click', onButtonIncrement);
+
+function handleButtonDecrement () {};
+function handleButtonIncrement () {};
