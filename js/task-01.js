e143f1eb52d957ee911643a7c23872270a39fe3f
Konvoir <baleskoff@gmail.com>
2021-06-09 13:03:31 +0300

    1,3

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,34 @@
-const sumCategories = document.querySelectorAll('.item').length;
+const itemsRef = document.querySelectorAll('.item');
+const sumCategories = itemsRef.length;
 const message = `В списке ${sumCategories} категории.`;
 
 console.log(message);
 
 
-const heddingText = document.querySelectorAll('.item');
+const heddingText = document.querySelectorAll('h2');
+
+for (let i = 0; i < sumCategories; i++) {
+   console.log("текст заголовка элемента:   ", heddingText[i].textContent)  
+   console.log("~ itemsRef.children", itemsRef[i].children);
+}
+   
+
+// console.log("~ heddingText", [...heddingText[0]].textContent);
 // const tegs = heddingText.children
 
 // текст заголовка
-const text = {... heddingText};
+// const text = {... heddingText};
 
-console.log(text);
+// console.log(text);
 
 
-const ara = text.forEach( e => e.firstElementChild);
-// const ara = text.map( e => e.firstElementChild);
-console.log("~ ara", ara)
+// const listTitle = document.querySelectorAll('h2').textContent
+// console.log("~ listTitle", listTitle)
+
+// const ara = text.forEach( e => e.firstElementChild);
+// const tara = text.map( e => e.firstElementChild);
+// console.log("~ tara", tara)
+// console.log("~ ara", ara)
+
+const ulCategories = document.querySelector('#categories');
+console.log("~ ulCategories", ulCategories.children)
--- js/task-03.js
+++ js/task-03.js
@@ -1,17 +1,13 @@
-Напиши скрипт для создания галлереи изображений по массиву данных.
 
-В HTML есть список `ul#gallery`.
+// <ul id="gallery"></ul>
 
-```html
-<ul id="gallery"></ul>
-```
 
-Используй массив объектов `images` для создания тегов `img` вложенных в `li`.
-Для создания разметки используй шаблонные строки и `insertAdjacentHTML()`.
+// Используй массив объектов `images` для создания тегов `img` вложенных в `li`.
+// Для создания разметки используй шаблонные строки и `insertAdjacentHTML()`.
 
-- Все элементы галереи должны добавляться в DOM за одну операцию вставки.
-- Добавь минимальное оформление галереи флексбоксами или гридами через
-  css-классы.
+// - Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// - Добавь минимальное оформление галереи флексбоксами или гридами через
+//   css-классы.
 
   const images = [
     {
@@ -27,6 +23,27 @@
       alt: 'Group of Horses Running',
     },
   ];
-
-
   
+  const mark = createImages(images);
+  console.log("~ createImages ", mark );
+function createImages (images) {
+  return images.map(({url, alt}) => {
+    return `<li> <img  src="${url}" alt="${alt}" > </li>`;
+  }).join('');
+  }; 
+
+   
+  const listRef = document.querySelector('#gallery');
+  console.log("~ listRef ", listRef )
+
+
+ listRef.insertAdjacentHTML('beforeend', mark);
+
+  const elements = images.map(e => {
+      const tegs = document.createElement('li');
+      tegs.textContent = e.url;
+      return tegs;
+  });
+  console.log("~ elements", elements);
+
+  // listRef.appendChild(elements);
