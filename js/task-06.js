3db23e4c4ecd6201fbbd13aa9ef5be5aa369ce37
Konvoir <baleskoff@gmail.com>
2021-06-09 16:27:34 +0300

    6

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,25 @@
 const refs = {
-    input: document.querySelector('#validation-input')
+    input: document.querySelector('input'),
+    inputValue:  document.querySelector('#validation-input')
 };
-console.log("~ refs", refs);
-
+const totalLength = refs.input.getAttribute('data-length');
 
 refs.input.addEventListener('blur', onInputBlur);
 
-function onInputBlur () {
-    refs.input.classList.add('invalid')
-    if () {refs.input.classList.remove('invalid');
-        refs.input.classList.add('valid')} ;
+function onInputBlur (evt) {
+    const val = (evt.target.value);
+  if (val.length >= totalLength) { valid()
+} else {inValid() };
 };
+
+    function valid (){
+    refs.input.classList.add('valid');
+    refs.input.classList.remove('invalid');
+    };
+
+    
+    function inValid (){
+        refs.input.classList.remove('valid');
+        refs.input.classList.add('invalid');
+        };
+
